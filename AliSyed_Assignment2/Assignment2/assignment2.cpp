3a0a58820f758f4cb960bf2ed046b6b6127f2b77
Asyed8200 <Asyed8200@conestogac.on.ca>
2022-09-21 15:44:46 -0400

    Step 3: Initial Source Code (Student List in Vector Space)

--- AliSyed_Assignment2/Assignment2/assignment2.cpp
+++ AliSyed_Assignment2/Assignment2/assignment2.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+typedef struct STUDENT_DATA {
+	string firstName;
+	string lastName;
+
+	string printStudent() {
+		string fullName = lastName + "," + firstName;
+		return fullName;
+	}
+}STUDENTDATA;
+
 int main() {
+
+	string tempDataStorage;
+	vector<STUDENTDATA>studentList;
+
+	ifstream StudentDataFile("..\\StudentData.txt");
+
+	while (getline(StudentDataFile, tempDataStorage)) {
+		int totalStringLength = tempDataStorage.length();
+		STUDENTDATA newStudent;
+
+		newStudent.lastName = tempDataStorage.substr(0, tempDataStorage.find(','));
+		newStudent.firstName = tempDataStorage.substr(tempDataStorage.find(',') + 1, totalStringLength);
+
+		studentList.push_back(newStudent);
+	}
+	
+	//for (int i = 0; i < studentList.size(); i++)
+	//	cout << studentList.at(i).printStudent() << endl;
+
+	StudentDataFile.close();
 	return 1;
 }
